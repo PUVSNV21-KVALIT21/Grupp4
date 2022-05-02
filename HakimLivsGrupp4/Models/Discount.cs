0b6538c8496452be9ee213155645fba42b612957
johanyderhag <75755768+johanyderhag@users.noreply.github.com>
2022-05-02 18:49:13 +0200

    Add Discount model (#12)

---
+++ HakimLivsGrupp4/Models/Discount.cs
@@ -0,0 +1,9 @@
+﻿namespace HakimLivsGrupp4.Models
+{
+    public class Discount
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public int Percentage { get; set; }
+    }
+}
