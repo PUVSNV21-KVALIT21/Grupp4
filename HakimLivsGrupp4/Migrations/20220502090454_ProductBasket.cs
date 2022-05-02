739238e49ca427a9f73527c1d784bce22653b80e
AThomasBring <73478932+AThomasBring@users.noreply.github.com>
2022-05-02 11:04:27 +0100

    Added BasketProducts

---
+++ HakimLivsGrupp4/Migrations/20220502090454_ProductBasket.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HakimLivsGrupp4.Migrations
+{
+    public partial class ProductBasket : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BasketProduct_Basket_BasketID",
+                table: "BasketProduct");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_BasketProduct",
+                table: "BasketProduct");
+
+            migrationBuilder.RenameTable(
+                name: "BasketProduct",
+                newName: "BasketProducts");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_BasketProduct_BasketID",
+                table: "BasketProducts",
+                newName: "IX_BasketProducts_BasketID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_BasketProducts",
+                table: "BasketProducts",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BasketProducts_Basket_BasketID",
+                table: "BasketProducts",
+                column: "BasketID",
+                principalTable: "Basket",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BasketProducts_Basket_BasketID",
+                table: "BasketProducts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_BasketProducts",
+                table: "BasketProducts");
+
+            migrationBuilder.RenameTable(
+                name: "BasketProducts",
+                newName: "BasketProduct");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_BasketProducts_BasketID",
+                table: "BasketProduct",
+                newName: "IX_BasketProduct_BasketID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_BasketProduct",
+                table: "BasketProduct",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BasketProduct_Basket_BasketID",
+                table: "BasketProduct",
+                column: "BasketID",
+                principalTable: "Basket",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ HakimLivsGrupp4/Migrations/20220502095745_UpdatedConnectionString.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HakimLivsGrupp4.Migrations
+{
+    public partial class UpdatedConnectionString : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
