67cd1bec88a6669a07c4fba7d159ced3ba971a46
axelbohlin <75880102+axelbohlin@users.noreply.github.com>
2022-05-03 16:42:07 +0200

    Added sidebar and CSS to .container

---
+++ HakimLivsGrupp4/Migrations/20220502123133_todb.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HakimLivsGrupp4.Migrations
+{
+    public partial class todb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Products");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryID",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryID",
+                table: "Products");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
