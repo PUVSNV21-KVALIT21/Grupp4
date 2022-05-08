10bcc328bca67f5db5be277b2920e60dde0504de
johanyderhag <75755768+johanyderhag@users.noreply.github.com>
2022-05-09 00:29:38 +0200

    Add shopping and modal and +/- buttons

--- HakimLivs/Models/CartState.cs
+++ HakimLivs/Models/CartState.cs
@@ -2,25 +2,63 @@
 {
     public class CartState
     {
-        public List<Product>? selectedProduct = new List<Product>();
+        public List<BasketProduct>? selectedProducts = new List<BasketProduct>();
 
+        public decimal? totalPrice { get; set; } = 0;
 
         public event Action OnChange;
-        //public List<Product> Products
-        //{
-        //    get => selectedProduct ?? null; 
-        //    set
-        //    {
-        //        selectedProduct.Add(value);
-        //        NotifyStateChanged();
-
-        //    }
-        //}
+      
         public void AddProduct(Product product)
         {
-            selectedProduct.Add(product);
+            BasketProduct basketProduct = new BasketProduct();
+
+            var isAlreadyInBasket = selectedProducts.Where(bp => bp.Product == product).SingleOrDefault(new BasketProduct());
+
+            if (isAlreadyInBasket.Product != null)
+            {
+                isAlreadyInBasket.ProductQuantity++;
+            }
+            else
+            {
+                isAlreadyInBasket.Product = product;
+                isAlreadyInBasket.ProductQuantity = 1;
+                selectedProducts.Add(isAlreadyInBasket);
+
+            }
+
+            totalPrice = totalPrice + product.Price;
+
             NotifyStateChanged();
         }
+
+        public void AddProductQuantity(BasketProduct basketProduct)
+        {
+            var product = selectedProducts.IndexOf(basketProduct);
+
+            selectedProducts[product].ProductQuantity++;
+            totalPrice = totalPrice + selectedProducts[product].Product.Price;
+
+            NotifyStateChanged();
+        }
+        public void SubtractProductQuantity(BasketProduct basketProduct)
+        {
+            var product = selectedProducts.IndexOf(basketProduct);
+
+            if (selectedProducts[product].ProductQuantity > 1)
+            {
+                selectedProducts[product].ProductQuantity--;
+                totalPrice = totalPrice - selectedProducts[product].Product.Price;
+
+            }
+            else
+            {
+                selectedProducts.Remove(selectedProducts[product]);
+            }
+
+            NotifyStateChanged();
+        }
+
+
 
         private void NotifyStateChanged() => OnChange?.Invoke();
 
