87856c86e812f5619103847cd48a29887df67a88
johanyderhag <75755768+johanyderhag@users.noreply.github.com>
2022-05-10 22:43:09 +0200

    Bug fixes, few layout changes and add local storage

---
+++ HakimLivs/Models/Discount.cs
@@ -0,0 +1,9 @@
+﻿namespace HakimLivs.Models
+{
+    public class Discount
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public int Percentage { get; set; }
+    }
+}
