dd4956ded6f48b120704f1c0ff4f2293f8dd94bd
axelbohlin <75880102+axelbohlin@users.noreply.github.com>
2022-05-12 11:27:00 +0200

    Register user with extended fields and age-restriction.

--- HakimLivs/Data/ApplicationDbContext.cs
+++ HakimLivs/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HakimLivs.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
@@ -18,5 +18,26 @@ namespace HakimLivs.Data
         public DbSet<Order> Orders { get; set; }
         public DbSet<Product> Products { get; set; }
 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .Property(e => e.firstName)
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .Property(e => e.lastName)
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .Property(e => e.Email)
+                .HasMaxLength(100);
+
+
+
+        }
     }
 }
