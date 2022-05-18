ec4b9e4dd9adc026c610d11cb696fc4e5e303d23
KippingMonkey <73753894+KippingMonkey@users.noreply.github.com>
2022-05-18 23:11:34 +0200

    make product card more responsive

--- HakimLivs/Data/ApplicationUser.cs
+++ HakimLivs/Data/ApplicationUser.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using HakimLivs.Models;
+using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -8,15 +9,23 @@ namespace HakimLivs.Data
     {
         [RegularExpression("^[a-öA-Ö-].*?$", ErrorMessage = "Bara bokstäver är tillåtna värden."), MaxLength(50)]
         public string firstName { get; set; }
+
         [RegularExpression("^[a-öA-Ö-].*?$", ErrorMessage = "Bara bokstäver är tillåtna värden."), MaxLength(50)]
         public string lastName { get; set; }
+
         [RegularExpression("^[a-öA-Ö0-9].*?$", ErrorMessage = "Bara bokstäver och siffror är tillåtna värden."), MaxLength(100)]
         public string Address { get; set; }
+
         [RegularExpression("^[a-öA-Ö0-9].*?$", ErrorMessage = "Bara bokstäver och siffror är tillåtna värden."), MaxLength(20)]
         public string Postcode { get; set; }
+
         [RegularExpression("^[a-öA-Ö-].*?$", ErrorMessage = "Bara bokstäver är tillåtna värden."), MaxLength(100)]
         public string City { get; set; }
+
         [Column(TypeName = "Date")]
         public DateTime Age { get; set; }
+
+        public List<Discount> UsedDiscounts { get; set; }
+
     }
 }
